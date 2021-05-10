a85798f31b25988e024ecc705b9bdd165505109a
yeonsunoh <dustn7322@g.shingu.ac.kr>
2021-05-10 12:21:24 +0900

    context오류

--- app/src/main/java/com/example/shingubotanic/spring.java
+++ app/src/main/java/com/example/shingubotanic/spring.java
@@ -3,15 +3,23 @@ package com.example.shingubotanic;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
 
 public class spring extends AppCompatActivity {
 
     ImageButton back;
     View.OnClickListener cl;
     Intent i;
+    MapView mapView = new MapView(this);
+
+    ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,8 @@ public class spring extends AppCompatActivity {
         setContentView(R.layout.spring);
 
         back = (ImageButton) findViewById(R.id.back);
+        mapViewContainer.addView(mapView);
+
 
         cl = new View.OnClickListener(){
             @Override
