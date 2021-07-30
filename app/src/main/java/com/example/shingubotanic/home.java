963df90f77aed816116c82f9a82e57c9a4a410b8
yeonsunoh <dustn7322@g.shingu.ac.kr>
2021-07-30 13:30:10 +0900

    홈화면_기본 틀 생성 및 xml, java파일 생성

--- app/src/main/java/com/example/shingubotanic/home.java
+++ app/src/main/java/com/example/shingubotanic/home.java
@@ -12,7 +12,7 @@ import androidx.appcompat.widget.Toolbar;
 public class home extends MainActivity {
     private String TAG = "VideoActivity";
 
-    ImageButton b1,b2,b3,b4,home;
+    ImageButton gui,wea,cou,info,shop,fore;
     View.OnClickListener cl;
     Intent i;
 
@@ -22,10 +22,54 @@ public class home extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        gui = (ImageButton) findViewById(R.id.guide);
+        wea = (ImageButton) findViewById(R.id.weathercourse);
+        cou = (ImageButton) findViewById(R.id.course);
+        info = (ImageButton) findViewById(R.id.info);
+        shop = (ImageButton) findViewById(R.id.shop);
+        fore = (ImageButton) findViewById(R.id.foreign);
+
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         Toolbar toolbar2 = (Toolbar)findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         setSupportActionBar(toolbar2);
 
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.guide:
+                        i = new Intent(getApplicationContext(), guide.class);
+                        startActivity(i);
+                        break;
+                    case R.id.weathercourse:
+                        i = new Intent(getApplicationContext(), weathercou.class);
+                        startActivity(i);
+                        break;
+                    case  R.id.course:
+                        i = new Intent(getApplicationContext(), course.class);
+                        startActivity(i);
+                        break;
+                    case R.id.info:
+                        i = new Intent(getApplicationContext(), info.class);
+                        startActivity(i);
+                        break;
+                    case R.id.shop:
+                        i = new Intent(getApplicationContext(), shop.class);
+                        startActivity(i);
+                        break;
+                    case R.id.foreign:
+                        i = new Intent(getApplicationContext(), foreign.class);
+                        startActivity(i);
+                        break;
+                }
+            }
+        };
+        gui.setOnClickListener(cl);
+        wea.setOnClickListener(cl);
+        cou.setOnClickListener(cl);
+        info.setOnClickListener(cl);
+        shop.setOnClickListener(cl);
+        fore.setOnClickListener(cl);
     }
 }
